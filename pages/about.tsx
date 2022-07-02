266ae0f24daacc1a8dbbfaa60e5d12c0729150ae
baotran13 <bao.tran@techupcorp.com>
2022-07-02 21:34:54 +0700

    eslint about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,6 @@ export interface AboutPageProps{
 
 }
 
-export default function AboutPage(props: AboutPageProps){
+export default function AboutPage(props: AboutPageProps) {
   return <div>About page</div>
 }
