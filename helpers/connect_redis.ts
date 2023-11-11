52d3ccc6793c303d68437fafb6332c600305761f
Trần Đình Bảo <71821328+baokuro04122000@users.noreply.github.com>
2023-11-11 22:29:00 +0700

    Update connect_redis.ts

--- helpers/connect_redis.ts
+++ helpers/connect_redis.ts
@@ -1,10 +1,10 @@
 import Redis from 'ioredis'
 
 const redis = new Redis({
-  port: 18798, // Redis port
-  host: "redis-18798.c73.us-east-1-2.ec2.cloud.redislabs.com", // Redis host
+  port: 15209, // Redis port
+  host: "redis-15209.c11.us-east-1-2.ec2.cloud.redislabs.com", // Redis host
   username: "default", // needs Redis >= 6
-  password: "2e4FKR4b5TYNgtNAdn2DqPqcxsQfXF3P"
+  password: "n8OOfny9Tn0IMjGkvQXK4hWWuVmRV5YU"
 })
 
 export default redis
